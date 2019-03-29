33d795d6984f0aa65b16648b0f1c07c70e01796b
Konstantin Brodin <brodin.konstantin@gmail.com>
2019-03-28 20:37:17 -0400

    Initial commit

---
+++ PDFpass/Program.cs
@@ -0,0 +1,44 @@
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFpass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open an existing document. Providing an unrequired password is ignored.
+            var filename = @"";
+            PdfDocument document = PdfReader.Open(filename, "some text");
+
+            PdfSecuritySettings securitySettings = document.SecuritySettings;
+
+            // Setting one of the passwords automatically sets the security level to 
+            // PdfDocumentSecurityLevel.Encrypted128Bit.
+            securitySettings.UserPassword = "aaaa";
+            securitySettings.OwnerPassword = "aaaa";
+
+            // Don't use 40 bit encryption unless needed for compatibility reasons
+            //securitySettings.DocumentSecurityLevel = PdfDocumentSecurityLevel.Encrypted40Bit;
+
+            // Restrict some rights.
+            securitySettings.PermitAccessibilityExtractContent = false;
+            securitySettings.PermitAnnotations = false;
+            securitySettings.PermitAssembleDocument = false;
+            securitySettings.PermitExtractContent = false;
+            securitySettings.PermitFormsFill = true;
+            securitySettings.PermitFullQualityPrint = false;
+            securitySettings.PermitModifyDocument = true;
+            securitySettings.PermitPrint = false;
+
+            // Save the document...
+            document.Save(@"");
+        }
+    }
+}
